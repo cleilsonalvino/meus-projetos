c42b7f72ae749efa5e87f9307ade94e07b34bea1
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2023-07-29 13:37:00 -0300

    Update javascript.js

--- calculadora/javascript/javascript.js
+++ calculadora/javascript/javascript.js
@@ -1,5 +1,20 @@
-let botao = document.createElement('button')
-
-botao.onclick = function() {
-    alert('kkkkk')
-}
+    
+      function inserir (num) {
+      var numero = document.getElementById('resultado').innerHTML;
+      document.getElementById('resultado').innerHTML = numero + num;
+      }
+      function clean () {
+        document.getElementById('resultado').innerHTML = ''
+      }
+      function apagarUm() {
+        var back = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = back.substring(0, back.length -1)
+      }
+      function calcular () {
+        var resultado = document.getElementById('resultado').innerHTML;
+        if (resultado) {
+          document.getElementById('resultado').innerHTML = eval(resultado);
+        } else {
+          document.getElementById('resultado').innerHTML = 'Valor inválido'
+        }
+      }
