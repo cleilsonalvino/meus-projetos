8c24eb0ee767217402c3f6f83e6997f3108cf288
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2023-10-29 20:10:58 -0300

    jogo do mario

---
+++ super-mario/script/script.js
@@ -0,0 +1,31 @@
+const mario = document.querySelector('.mario')
+const tubo = document.querySelector('.tubo')
+
+
+
+const jump = ()=>{
+    mario.classList.add('jump') // adiciona a class jump
+
+    setTimeout(()=>{
+        mario.classList.remove('jump') //remove a class jump
+    },500)
+}
+
+const loop = setInterval(()=>{
+    const tuboPosition = tubo.offsetLeft; // pega o valor do deslocamento da esquerda do tubo
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); //pega o valor da posicao do mario e o replace com o  + transforma o valor de string para numero
+    
+    if(tuboPosition <= 50 && tuboPosition > 0 && marioPosition < 50 ) {
+        tubo.style.animation = 'none'
+        tubo.style.left = `${tuboPosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = 'imagens/game-over.png'
+        mario.style.width = '35px'
+        mario.style.marginLeft = '15px'
+    }
+}, 10)
+
+document.addEventListener('keydown', jump)
